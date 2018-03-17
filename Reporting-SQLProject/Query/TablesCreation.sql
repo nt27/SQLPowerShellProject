83759e53006c055e8006ed2fa97bc84254f21f63
Navaneethakrishnan Thangaraj <navaneethakrishnan.thangaraj@outlook.com>
2018-03-17 20:47:16 +0000

    Updated

--- Reporting-SQLProject/Query/TablesCreation.sql
+++ Reporting-SQLProject/Query/TablesCreation.sql
@@ -5,6 +5,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+If Not Exists(Select 'X' From Sysobjects Where name = 'Product')
+Begin
 CREATE TABLE [dbo].[Product](
 	[ProductID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](50) NOT NULL,
@@ -20,24 +22,30 @@ CREATE TABLE [dbo].[Product](
 	[SellEndDate] [datetime] NULL,
 	[ListPrice] [money] NOT NULL
 ) ON [PRIMARY]
+End
 GO
 /****** Object:  Table [dbo].[ProductCategory]    Script Date: 3/17/2018 6:19:33 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+If Not Exists(Select 'X' From Sysobjects Where name = 'ProductCategory')
+Begin
 CREATE TABLE [dbo].[ProductCategory](
 	[ProductCategoryID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](50) NOT NULL,
 	[rowguid] [uniqueidentifier] NOT NULL,
 	[ModifiedDate] [datetime] NOT NULL
 ) ON [PRIMARY]
+End
 GO
 /****** Object:  Table [dbo].[ProductSubcategory]    Script Date: 3/17/2018 6:19:33 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+If Not Exists(Select 'X' From Sysobjects Where name = 'ProductSubcategory')
+Begin
 CREATE TABLE [dbo].[ProductSubcategory](
 	[ProductSubcategoryID] [int] IDENTITY(1,1) NOT NULL,
 	[ProductCategoryID] [int] NOT NULL,
@@ -45,4 +53,5 @@ CREATE TABLE [dbo].[ProductSubcategory](
 	[rowguid] [uniqueidentifier] NOT NULL,
 	[ModifiedDate] [datetime] NOT NULL
 ) ON [PRIMARY]
+End
 GO
